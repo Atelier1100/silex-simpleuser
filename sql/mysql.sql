c45e1f62772d7ef8f3d23f40c7ad27c50c95d29d
Atelier1100 <Atelier1100@users.noreply.github.com>
2014-04-28 10:31:15 +0200

    Update mysql.sql
    Remove name column

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -4,7 +4,6 @@ CREATE TABLE `users` (
   `password` VARCHAR(255) NOT NULL DEFAULT '',
   `salt` VARCHAR(255) NOT NULL DEFAULT '',
   `roles` VARCHAR(255) NOT NULL DEFAULT '',
-  `name` VARCHAR(100) NOT NULL DEFAULT '',
   `time_created` INT NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_email` (`email`)
